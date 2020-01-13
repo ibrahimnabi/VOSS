c13ccb992d67d89805d996df3cd6603f5667c31c
unknown <ibrahimnabi80@gmail.com>
2020-01-13 11:35:39 +0500

    Updates

---
+++ precache-manifest.fe8075a32a37597f9866eedff692f79b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4410228db7c337aee15b7748d415b4",
+    "url": "/VOSS/index.html"
+  },
+  {
+    "revision": "8fc4d7a3f7ef7b5a0243",
+    "url": "/VOSS/static/css/main.f3958e5a.chunk.css"
+  },
+  {
+    "revision": "e9091512e669c4e8bfe7",
+    "url": "/VOSS/static/js/2.f091e6d9.chunk.js"
+  },
+  {
+    "revision": "5586a95853ee43deda1fb4da37dc3fc8",
+    "url": "/VOSS/static/js/2.f091e6d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fc4d7a3f7ef7b5a0243",
+    "url": "/VOSS/static/js/main.a088d463.chunk.js"
+  },
+  {
+    "revision": "e6c837a27e3034cd4cdd",
+    "url": "/VOSS/static/js/runtime-main.3104ab8b.js"
+  },
+  {
+    "revision": "12f98d8bd3166de38cdab9163f3e4017",
+    "url": "/VOSS/static/media/logo.12f98d8b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VOSS/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efdf9599802ea1a62e279a2e54b63123.js"
+  "/VOSS/precache-manifest.fe8075a32a37597f9866eedff692f79b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/VOSS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
